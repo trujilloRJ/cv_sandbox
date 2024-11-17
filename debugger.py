7cddb4d27b4f31bb9a3af561b77b85928a9fcf67
Javier Trujillo Rodriguez <trujillorodriguezjavier@gmail.com>
2024-11-17 09:20:18 +0100

    added gt viz to debugger

--- debugger.py
+++ debugger.py
@@ -2,7 +2,7 @@ import cv2 as cv
 import pandas as pd
 import numpy as np
 from os import listdir
-from utils import KEY_A, KEY_ESC, KEY_D, KEY_M, KEY_N, draw_detection_bb, draw_track_bb
+from utils import KEY_A, KEY_ESC, KEY_D, KEY_M, KEY_N, draw_detection_bb, draw_track_bb, draw_gt_bb
 from configuration import get_config
 
 
@@ -10,26 +10,39 @@ SEQUENCE = "0000"
 IMG_PATH = f"data/images/{SEQUENCE}/"
 DET_FILE = f"data/nms/{SEQUENCE}.csv"
 TRACK_FILE = f"data/tracks/{SEQUENCE}.txt"
+GT_FILE = f"data/gt/{SEQUENCE}.txt"
 DATASET = 'KITTI'
 
-def load_img(frame_name, dets, show_dets, show_tracks):
+def load_img(frame_name, dets, tracks, gt, show_dets, show_tracks, show_gt):
     frame_index = int(frame_name[:-4])
     frame = cv.imread(IMG_PATH + frame_name, cv.IMREAD_COLOR)
-    cur_dets = dets.loc[dets['frame'] == frame_index, :]
 
-    # drawing
+    cur_dets = dets.loc[dets['frame'] == frame_index, :]
+    cur_tracks = tracks.loc[(tracks['frame'] == frame_index), :]
+    cur_gt = gt.loc[(gt['frame'] == frame_index), :]
+
+    # drawing dets
     if show_dets:
         for det_id in cur_dets.index:
             det = cur_dets.loc[det_id, :]
             draw_detection_bb(frame, det)
 
+    # drawing track bounding box
     if show_tracks:
-        # cur_tracks = tracks.loc[(tracks['frame'] == frame_index) & (tracks['tentative'] == False), :]
-        cur_tracks = tracks.loc[(tracks['frame'] == frame_index), :]
-        # drawing track bounding box
         for _, track in cur_tracks.iterrows():
             track_bb = [int(track['top']), int(track['left']), int(track['bottom']), int(track['right'])]
             draw_track_bb(frame, track['id'], track['type'], track_bb)
+
+    # drawing track bounding box
+    if show_gt:
+        shapes = np.zeros_like(frame, np.uint8)
+        for _, gt_ in cur_gt.iterrows():
+            bb = [int(gt_['top']), int(gt_['left']), int(gt_['bottom']), int(gt_['right'])]
+            draw_gt_bb(shapes, gt_['id'], gt_['type'], bb)
+        alpha = 0.8
+        mask = shapes.astype(bool)
+        frame[mask] = cv.addWeighted(frame, alpha, shapes, 1 - alpha, 0)[mask]
+
 
     cv.putText(frame, f'{frame_name}', (50, 50), cv.FONT_HERSHEY_SIMPLEX, 2, (255, 255, 255))
 
@@ -38,14 +51,13 @@ def load_img(frame_name, dets, show_dets, show_tracks):
 
 if __name__ == '__main__':
     
-    show_dets = True
+    show_dets = False
     show_tracks = True
+    show_gt = True
 
     config = get_config(DATASET)
     frames = listdir(IMG_PATH)
 
-    # dets = pd.read_csv(DET_FILE, header=None, sep=" ")
-    # dets.columns = config['det_cols']
     dets = pd.read_csv(DET_FILE, sep=' ')
 
     tracks = pd.read_csv(TRACK_FILE, sep=' ')
@@ -53,12 +65,16 @@ if __name__ == '__main__':
     cols_to_round = ['left', 'top', 'right', 'bottom']
     tracks[cols_to_round] = np.round(tracks[cols_to_round]).astype(int)
 
+    gt = pd.read_csv(GT_FILE, sep=' ')
+    gt.columns = config['track_cols'][:-1]
+    gt[cols_to_round] = np.round(gt[cols_to_round]).astype(int)
+
     cur_frame = 0
     run = True
 
     while(run):
         frame_name = frames[cur_frame]
-        frame = load_img(frame_name, dets, show_dets, show_tracks)
+        frame = load_img(frame_name, dets, tracks, gt, show_dets, show_tracks, show_gt)
 
         cv.imshow("display", frame)
         key = cv.waitKey(0)
--- utils.py
+++ utils.py
@@ -14,8 +14,12 @@ COLOR_DET = {
     OBJ_TYPES['Pedestrian'] : (255,0,0),
     OBJ_TYPES['Cyclist'] : (255,255,0),
 }
-COLOR_TRACK = (0,0,255)
+COLOR_TRACK = (0, 0, 255)
+COLOR_GT = (0, 255, 0)
 
+def draw_gt_bb(frame, gt_id, gt_type, gt_bb):
+    cv.rectangle(frame, (gt_bb[1], gt_bb[0]), (gt_bb[3], gt_bb[2]), COLOR_GT, cv.FILLED)
+    cv.putText(frame, f'{gt_id}:{gt_type}', (int((gt_bb[1] + gt_bb[3])/2), gt_bb[0]-10), cv.FONT_HERSHEY_SIMPLEX, 0.5, COLOR_GT)
 
 def draw_track_bb(frame, track_id, track_type, track_bb):
     cv.rectangle(frame, (track_bb[1], track_bb[0]), (track_bb[3], track_bb[2]), COLOR_TRACK, 1)
